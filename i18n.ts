0908297f1f48033a40d4dcc24ed37ba5656c32be
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-08-24 19:30:41 -0300

    Fix: change i18n

--- i18n.ts
+++ i18n.ts
@@ -4,11 +4,13 @@ import { notFound } from 'next/navigation';
 export const locales = ['en', 'pt-br'];
 
 export default getRequestConfig(async ({ locale }) => {
+  // A verificação de segurança continua a mesma
   if (!locales.includes(locale as any)) {
     notFound();
   }
 
   return {
+    locale,
     messages: (await import(`./src/messages/${locale}.json`)).default,
   };
 });
