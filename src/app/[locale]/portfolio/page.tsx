161a18be61d051ad037eea618bbe9e4073a2d036
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-08-24 19:49:45 -0300

    Fix: Type error Property

--- src/app/[locale]/portfolio/page.tsx
+++ src/app/[locale]/portfolio/page.tsx
@@ -1,18 +1,50 @@
 import { ProjectsCarousel } from '@/components/ProjectsCarousel/ProjectsCarousel';
 import ContactSection from '@/components/Sections/ContactSection';
 import { getTranslations } from 'next-intl/server';
+import { setRequestLocale } from 'next-intl/server';
 import { Project } from '@/types/project-types';
 
-export default async function PortfolioPage() {
-  const tPortfolio = await getTranslations('PortfolioPage');
-  const tProjectsData = await getTranslations('ProjectDetailsPage');
-  const tContact = await getTranslations('ContactSection');
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: 'PortfolioPage' });
+
+  return {
+    title: t('title'),
+  };
+}
+
+export default async function PortfolioPage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+
+  setRequestLocale(locale);
+
+  const tPortfolio = await getTranslations({
+    locale,
+    namespace: 'PortfolioPage',
+  });
+  const tProjectsData = await getTranslations({
+    locale,
+    namespace: 'ProjectDetailsPage',
+  });
+  const tContact = await getTranslations({
+    locale,
+    namespace: 'ContactSection',
+  });
 
   const projectsObject = tProjectsData.raw('projects');
   const projectsArray = Object.values(projectsObject) as Project[];
 
   const contactTranslations = {
     mainHeading: tContact('mainHeading'),
+    subHeading: tContact('subHeading'), // <- Esta linha estava faltando
     description: tContact('description'),
     ctaButton: tContact('ctaButton'),
     emailAddress: tContact('emailAddress'),
