f9370386bfc453066c03fd34c9261e97502f9d48
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-08-24 19:46:01 -0300

    Fix: type error log

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,5 +1,6 @@
 import ContactSection from '@/components/Sections/ContactSection';
 import { getTranslations } from 'next-intl/server';
+import { setRequestLocale } from 'next-intl/server';
 
 export async function generateMetadata({
   params,
@@ -7,13 +8,33 @@ export async function generateMetadata({
   params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
-  const t = await getTranslations({ locale, namespace: 'ContactPage' });
+  const t = await getTranslations({ locale, namespace: 'ContactSection' });
 
   return {
     title: t('title'),
   };
 }
 
-export default function ContactPage() {
-  return <ContactSection />;
+export default async function ContactPage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+
+  setRequestLocale(locale);
+
+  const t = await getTranslations({ locale, namespace: 'ContactSection' });
+
+  return (
+    <ContactSection
+      translations={{
+        mainHeading: t('mainHeading'),
+        subHeading: t('subHeading'),
+        description: t('description'),
+        ctaButton: t('ctaButton'),
+        emailAddress: t('emailAddress'),
+      }}
+    />
+  );
 }
