47586c605a65fec9c1ed2701e5b6b074cf0286fb
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-04-24 22:56:33 -0300

    fix: Download Button home

--- src/components/Sections/HomeSection.tsx
+++ src/components/Sections/HomeSection.tsx
@@ -19,29 +19,31 @@ export default function HomeSection({ translations }: HomeSectionProps) {
       className="flex items-center justify-center scroll-smooth px-5 pt-20 pb-30 md:px-10"
     >
       <div className="mx-auto flex w-fit flex-col">
-        <div className="flex justify-start py-5">
-          <Link href="#about" aria-label={translations.avatarAriaLabel}>
-            <Avatar className="md:min-h-10 md:min-w-10">
-              <AvatarImage
-                src="/images/foto-perfil.jpg"
-                alt={translations.avatarAlt}
-              />
-              <AvatarFallback>GB</AvatarFallback>
-            </Avatar>
-          </Link>
-        </div>
         <div className="lg:flex lg:gap-8">
-          <h1 className="dark:text-white-pure text-dark-black mb-8 py-1 text-xl font-light md:text-2xl lg:text-2xl">
-            {translations.greeting}
-          </h1>
+          <div>
+            <div className="flex justify-start py-5">
+              <Link href="#about" aria-label={translations.avatarAriaLabel}>
+                <Avatar className="md:min-h-10 md:min-w-10">
+                  <AvatarImage
+                    src="/images/foto-perfil.jpg"
+                    alt={translations.avatarAlt}
+                  />
+                  <AvatarFallback>GB</AvatarFallback>
+                </Avatar>
+              </Link>
+            </div>
+            <h1 className="dark:text-white-pure text-dark-black mb-8 py-1 text-xl font-light md:text-2xl lg:text-2xl">
+              {translations.greeting}
+            </h1>
+          </div>
           <div className="lg:flex lg:flex-col">
             <h2 className="text-dark-black dark:text-white-pure text-2xl font-bold md:text-7xl lg:text-7xl">
               {translations.mainTitle}
             </h2>
-            <div className="flex flex-col gap-6">
-              <DownloadCVButton text={translations.cvButton} />
-            </div>
           </div>
+        </div>
+        <div className="flex flex-col gap-6">
+          <DownloadCVButton text={translations.cvButton} />
         </div>
       </div>
     </section>
